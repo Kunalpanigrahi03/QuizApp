fe48745a67a1f1d2c80d90105bfc51a06fc54c41
Kunal <Kunal@codewithkunal.com>
2022-12-22 21:57:06 +0530

    Added Up the Questions

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNext : Button = findViewById(R.id.btnNext)
+        val etname : EditText = findViewById(R.id.et_name)
+        btnNext.setOnClickListener{
+            if (etname.text.isNotEmpty()){
+                Toast.makeText(this,"Please Enter your name",Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this,QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
